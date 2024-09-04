98073e4c39f507264ff0fecec0f8dbf9bd262bd4
陈唯实 <chen.wei.shi@icloud.com>
2024-09-04 14:02:31 +0800

    暂存

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
+import { Button } from 'antd';
+
+import { Calendar } from 'antd';
+
+function onPanelChange(value, mode) {
+  console.log(value, mode);
+}
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <br />
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+      </>
+
+      <br />
+
+      <Calendar onPanelChange={onPanelChange} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// 替换src/index.js里的内容：
+import React from 'react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// 18
+
+// import ReactDOM from 'react-dom/client'
+
+// 引入antDesign
+import 'antd/dist/antd.css';
+
+// 17
+
+import ReactDOM from 'react-dom'
+
+import './index.css'
+
+import App from './App'
+
+import reportWebVitals from './reportWebVitals'
+
+// 18
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(
+
+// <React.StrictMode>
+
+//           <App />
+
+// </React.StrictMode> // );
+
+// 17
+
+ReactDOM.render(
+
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />
+
+  </React.StrictMode>,
+
+  document.getElementById('root')
+
+)
+
